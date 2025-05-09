2b5583dfcc28a1cc713991fd7236cd85cb314f95
rostipostik <roro6178201@gmail.com>
2025-05-09 15:00:11 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+
+function App() {
+
+  return (
+    <>
+      <h1>react moai</h1>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
