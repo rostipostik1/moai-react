aec066c479f700bfb7c3b6dc762060ad68751628
rostipostik <roro6178201@gmail.com>
2025-05-21 16:56:24 +0100

    commit
    add header fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Nav } from '../nav/Nav'
 import logo from '../../assets/logo.png'
 import './header.scss'
 
 const Header = () => {
   console.log('hello');
-  
+  const [fixed, setFixed] = useState(false)
+  const handleScroll = () => {
+    setFixed(window.scrollY > 50)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    console.log('afrer useefect');
+    
+  }, [])
   return (
             
     <header className="header header-gradient">
-        <div className="header-wrapper">
+        <div className={`header-wrapper ${fixed ? 'fixed': ''}`}>
             <div className="container">
                 <div className="header-fixed">
                     <a href="" className="header-logo"><img src={logo} alt="logo"/></a>
